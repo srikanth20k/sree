c209f6e536039dd9f0aa55066871f3d6d36975f9
srikanth Adipireddy <srikanthreddya345@gmail.com>
2025-01-03 18:29:24 +0530

    Add files via upload

---
+++ TASK-2/first_st.py
@@ -0,0 +1,11 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+# import streamlit as st
+import altair as alt
+
+
+# data = pd.read_csv(r'C:\Users\SRIKANTH ADIPIREDDY\TASK-2\QVI_data.csv')
+st.title("money collecting app ")
+st.
